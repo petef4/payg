fd91a1ce79149a2ecce238a4640f499b6262a30f
Pete Forman <petef4@gmail.com>
2015-11-21 15:36:17 +0000

    unit tests

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+import flaskapp
+
+@pytest.fixture
+def app():
+    app = flaskapp.app
+    return app
---
+++ test_flaskapp.py
@@ -0,0 +1,20 @@
+from flask import url_for
+
+def test_home(client):
+    assert client.get(url_for('home')).status_code == 200
+
+
+def test_payg(client):
+    assert client.get(url_for('payg')).status_code == 200
+
+
+def test_links(client):
+    assert client.get(url_for('links')).status_code == 200
+
+
+def test_shopping(client):
+    assert client.get(url_for('shopping')).status_code == 200
+
+
+def test_test(client):
+    assert client.get(url_for('test')).status_code == 200
