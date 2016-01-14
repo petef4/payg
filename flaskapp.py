d24ca87ef0980ef3e346151d6990e074c1818a36
Pete Forman <petef4@gmail.com>
2016-01-14 21:49:57 +0000

    bing site verification

--- flaskapp.py
+++ flaskapp.py
@@ -59,6 +59,7 @@ def shopping():
 @app.route('/robots.txt')
 @app.route('/favicon.ico')
 @app.route('/google2427ff77d057f518.html')
+@app.route('/BingSiteAuth.xml')
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
